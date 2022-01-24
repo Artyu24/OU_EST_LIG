e8c7027ce7f2b507f212b0fe2b7a5ef6b63e15a0
Freddy <freddy_maupou@orange.fr>
2022-01-24 17:56:05 +0100

    ButtonsSpawned

---
+++ Assets/Data/RoundData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RoundData", menuName = "Data/RoundData", order = 1)]
+public class RoundData : ScriptableObject
+{
+    public Sprite[] spritesToFind;
+    public int[] buttonsNumber = new int[5];
+}
--- Assets/Script/Freddy/ButtonsSpawnManager.cs
+++ Assets/Script/Freddy/ButtonsSpawnManager.cs
@@ -7,6 +7,38 @@ public class ButtonsSpawnManager : MonoBehaviour
 {
     public List<Button> buttons = new List<Button>();
     public Sprite imgToClick;
+    int round = 1;
+    private RoundData roundData;
+    public Button wrongButton;
+    public Button rightButton;
+    public Canvas canvas;
+
+    private void Update()
+    {
+        switch (round)
+        {
+            case 1:
+                for(int i = 0; i < 50; i++)
+                {
+                    buttons.Add(wrongButton);
+                }
+                buttons.Add(rightButton);
+                SpawnButtons();
+                round = -1;
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void SpawnButtons()
+    {
+        Vector2 spawnButtons = new Vector2(50, 100);
+        foreach(Button button in buttons)
+        {
+            Instantiate(button, spawnButtons, Quaternion.identity, canvas.transform);
+        }
+    }
 
     public void OnClick()
     {
@@ -16,6 +48,10 @@ public class ButtonsSpawnManager : MonoBehaviour
             {
                 Debug.Log("Nice one !");
                 break;
+            }
+            else
+            {
+                Debug.Log("Nul !");
             }
         }
     }
